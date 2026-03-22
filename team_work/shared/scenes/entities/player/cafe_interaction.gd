206b658afec409acdfc72a105688b39c4dc08a91
uriella-o <okaforclarechimdi@gmail.com>
2026-03-22 09:55:16 +0000

    cafe

---
+++ team_work/shared/scenes/entities/player/cafe_interaction.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var mocha_button = $ColorRect/Panel/mochabutton
+@onready var matcha_button = $ColorRect/Panel/Matchabutton
+@onready var thank_you_label = $ColorRect/Panel/thankyoulabel
+@onready var sub_label = $ColorRect/Panel/sublabel
+
+func _ready():
+	thank_you_label.visible = false
+	mocha_button.pressed.connect(_on_mocha_pressed)
+	matcha_button.pressed.connect(_on_matcha_pressed)
+
+func _on_mocha_pressed():
+	show_thank_you("mocha", "2hrs")
+
+func _on_matcha_pressed():
+	show_thank_you("matcha", "4hrs")
+
+func show_thank_you(drink: String, time: String):
+	mocha_button.visible = false
+	matcha_button.visible = false
+	sub_label.visible = false
+	thank_you_label.visible = true
+	thank_you_label.text = "Thank you for ordering a " + drink + "!\nCome back next time for new drinks.\nLeaving in 4 seconds..."
+	await get_tree().create_timer(2.0).timeout
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
