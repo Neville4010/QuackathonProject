7899e568e689fd26ef1dbce29a275b486aa5a9c3
uriella-o <okaforclarechimdi@gmail.com>
2026-03-22 02:39:17 +0000

    idle sprites

--- team_work/shared/scenes/entities/player/cafe_owner.gd
+++ team_work/shared/scenes/entities/player/cafe_owner.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready():
 	animation_player.play("idle_down")
